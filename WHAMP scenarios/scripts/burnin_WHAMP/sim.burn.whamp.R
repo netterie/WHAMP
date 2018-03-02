ac8a0f4fb7c72fc78771859f3b28ef58e46113d6
darcyrao <darcy.rao@gmail.com>
2018-03-01 17:00:23 -0800

    Revise comments

--- WHAMP scenarios/scripts/burnin_WHAMP/sim.burn.whamp.R
+++ WHAMP scenarios/scripts/burnin_WHAMP/sim.burn.whamp.R
@@ -26,19 +26,14 @@ param <- param_msm_whamp(nwstats = st,
                    hiv.uct.rr = 1.73,
                    hiv.dual.rr = 0.2)
 init <- init_msm_whamp(nwstats = st)
-# Use this code for control when debugging on csde or home machine
-control <- control_msm_whamp(nsteps = 10,
-                             nsims = 1,
+# Use this code for control when running on csde or home machine
+control <- control_msm_whamp(nsteps = 3120,
+                             nsims = 16, ncores=16,
                              verbose = FALSE)
-## Use this version of the control code when running simulations on Hyak
-# control <- control_msm_whamp(simno = fsimno,
-#                        nsteps = 3120,
-#                        nsims = 16, ncores = 16,
-#                        verbose = FALSE)
 
 ## Simulation
-  # Use this version when running simulations on CSDE or home machine to test and de-bug
-  netsim(est, param, init, control)
+  # Use this version when running simulations on CSDE or home machine
+  sim1 <- netsim(est, param, init, control)
 
   ## Use this code when running simulations on Hyak
   # netsim_hpc(est, param, init, control, verbose = FALSE)
