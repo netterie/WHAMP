2c12f94ce014189daf20e13c336e3dd3d526fde7
darcyrao <darcy.rao@gmail.com>
2018-08-16 14:11:17 -0700

    Add target stats to check

--- Parameter estimation/Notes and draft files/Identify_ergm_target_stats.R
+++ Parameter estimation/Notes and draft files/Identify_ergm_target_stats.R
@@ -8,6 +8,10 @@ nw <- network.initialize(n = n, directed = FALSE)
 
 # Set up the attributes relevant to the model term of interest
 nw <- set.vertex.attribute(nw, attrname = "qntbyage", value = rep(c("Y1", "Y2", "Y3", "Y4", "O1", "O2", "O3", "O4"), each = n/8))
+nw <- set.vertex.attribute(nw, attrname = "region", value = rep(c("KC", "OW", "EW"), each = n/3))
+nw <- set.vertex.attribute(nw, attrname = "race", value = rep(c("H", "B", "O"), each = n/3))
+
 
 # Fill in the model equation with the terms of interest and it will indicate which target stats are expected and in what order
 summary(nw ~ edges + nodefactor("qntbyage"))
+summary(nw ~ edges + nodefactor("race") + nodefactor("region") + nodematch("race", diff=TRUE))
